1e30ac2dfe12f4645f570d342805c5650685c1b8
João Bispo <vitor.vi10@hotmail.com>
2020-10-12 18:04:42 -0300

    :lipstick: style: landing-page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
 
 import './styles/global.css';
 import './styles/pages/landing.css';
 
+import logoImg from './images/logo-marca.svg';
+
+
 function App() {
   return (
     <div id="page-landing">
-      <h1>
-      O meu encontro ontem foi incrível, Obrigado amor 💓
-      </h1>
+      <div className="content-wrapper">
+
+        <img src={logoImg} alt="Logo marca Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo.</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>São Paulo</strong>
+          <span>São Paulo</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)"/>
+        </a>
+      </div>
+
     </div>
   );
 }
