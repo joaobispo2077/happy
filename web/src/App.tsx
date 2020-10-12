0f83fd28aa68e6fec76368d6be74bb03494bafaf
João Bispo <vitor.vi10@hotmail.com>
2020-10-12 16:59:04 -0300

    :paperclip: chore: add img & create global style

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import './styles/global.css';
+import './styles/pages/landing.css';
 
 function App() {
   return (
-    <div>
+    <div id="page-landing">
+      <h1>
       O meu encontro ontem foi incrível, Obrigado amor 💓
+      </h1>
     </div>
   );
 }
