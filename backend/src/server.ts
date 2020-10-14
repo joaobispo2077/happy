aad3985fcc81c599558ed2aa88fb801a0734d0d7
João Bispo <vitor.vi10@hotmail.com>
2020-10-14 18:09:05 -0300

    :rocket: feat: catching exceptions

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import path from 'path';
+import 'express-async-errors';
 
 import './database/connection';
+
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
@@ -10,5 +13,6 @@ app.use(express.json());
 
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(routes);
+app.use(errorHandler);
 
 app.listen(3333);
