2432975de4555f871c480c23838e0097357bfb66
João Bispo <vitor.vi10@hotmail.com>
2020-10-14 18:29:34 -0300

    :rocket: feat: data validation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,12 +3,14 @@ import path from 'path';
 import 'express-async-errors';
 
 import './database/connection';
+import cors from 'cors';
 
 import routes from './routes';
 import errorHandler from './errors/handler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
