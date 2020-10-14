98de7b8ee91d4113076b6bee427d7bdb02d63d80
João Bispo <vitor.vi10@hotmail.com>
2020-10-14 11:59:14 -0300

    :rocket: feat: create orphanage w/o image

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanages';
 
 import './database/connection';
 
@@ -6,11 +8,32 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/orphanages', (req, res) => {
-  const body  = req.body;
-  const data = body;
+app.post('/orphanages', async (req, res) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  }  = req.body;
 
-  return res.status(201).json(data);
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  });
+
+  const orphanageCreated = await orphanagesRepository.save(orphanage);
+
+  return res.status(201).json(orphanageCreated);
 })
 
 app.listen(3333);
