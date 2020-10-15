cdb6294a0108575701b761a853d3ff013955e13e
João Bispo <vitor.vi10@hotmail.com>
2020-10-15 15:44:03 -0300

    :lipstick: style: marker on map

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link }from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 import '../styles/pages/orphanages-map.css';
 
-
 import mapMarkerImg from '../images/map-marker.svg';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+});
 
 function OrphanagesMap() {
   return(
@@ -33,6 +40,20 @@ function OrphanagesMap() {
       >
         {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
         <TileLayer url={`http://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+
+        <Marker icon={mapIcon} position={[-23.4449007,-46.5837066]}>
+          <Popup 
+          closeButton={false}
+          minWidth={240}
+          maxWidth={240}
+          className="map-popup"
+          >
+            Lar Criança frei
+            <Link to="">
+              <FiArrowRight size={20} color="#FFF"/>
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
 
       <Link to="" className="create-orphanage">
