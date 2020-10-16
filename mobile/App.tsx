1bc94e10b668567c7d04c2fa2aafcfc1146bfceb
João Bispo <vitor.vi10@hotmail.com>
2020-10-16 17:40:20 -0300

    :rocket: feat: routing and navigation

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
-import MapView, { Marker, Callout, PROVIDER_GOOGLE} from 'react-native-maps';
-import { Feather } from '@expo/vector-icons';
+
+import { StatusBar } from 'expo-status-bar';
 import { useFonts } from 'expo-font';
 import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
 
-import mapMarker from './src/images/map-marker.png';
+import Routes from './src/routes';
 
 export default function App() {
 
@@ -20,107 +19,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={{
-          latitude: -23.4560427,
-          longitude: -46.6003577,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
-        }}
-        > 
-          <Marker 
-            icon={mapMarker}
-            calloutAnchor={{
-              x: 2.7,
-              y: 0.8,
-            }}
-            coordinate={{
-              latitude: -23.4560427,
-              longitude: -46.6003577,
-            }}
-          >
-            <Callout tooltip={true} onPress={() => alert('i love Viviane so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much  so much ')}> 
-              <View style={styles.calloutContainer}>
-                <Text style={styles.calloutText}>Amo a Viviane</Text>
-              </View>
-            </Callout>
-          </Marker>
-        </MapView>
-
-        <View style={styles.footer}>
-          <Text style={styles.footerText}> Quero abraçar você Viviane... </Text>
-
-          <TouchableOpacity style={styles.createOrphanageButton} onPress={() => alert('Hey, you wanna drink')}>
-            <Feather name="plus" size={20} color={'#FFF'} />
-          </TouchableOpacity>
-        </View>
-    </View>
+    <>
+      <Routes />
+      <StatusBar  style="dark"/>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-
-  map: {
-    flex: 1,
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-
-  calloutContainer: {
-    width: 160,
-    height: 46,
-    paddingHorizontal: 16,
-
-    backgroundColor: 'rgba(255,255,255, 0.8)',
-    borderRadius: 16,
-
-    justifyContent: 'center',
-
-  },
-
-  calloutText: {
-    fontFamily: 'Nunito_700Bold',
-    color: '#0089a5',
-    fontSize: 14,
-  },
-
-  footer: {
-    position: "absolute",
-    left: 24,
-    right: 24,
-    bottom: 32,
-
-    backgroundColor: '#FFF',
-    borderRadius: 20,
-    height: 56,
-    paddingLeft: 24,
-
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-
-    elevation: 15,
-  },
-
-  footerText: {
-    fontFamily: 'Nunito_700Bold',
-    color: '#8fa7b3'
-  },
-
-  createOrphanageButton: {
-    width: 56,
-    height: 56,
-    backgroundColor: '#15c3d6',
-    borderRadius: 20,
-
-    justifyContent: 'center',
-    alignItems: 'center'
-
-  }
-});
---
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text,  StyleSheet } from 'react-native';
+
+export default function OrphanageDetails(){
+  return(
+    <Text style={styles.text}>Oioioi Viviane, fiz icon e loading e amo você demais</Text>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  text: {
+    flex: 1,
+    fontSize: 40,
+    textAlign: "center",
+    marginTop: 250
+  }
+});
